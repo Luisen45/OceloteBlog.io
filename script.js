879256f256c58edc865b4896acdee5a14a9bde14
Luisen45 <enriquecastanon54@gmail.com>
2024-10-22 21:30:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-JS;
-window.onload = function () {
+// Asegurarse de que el código se ejecute cuando la página esté cargada
+window.onload = function() {
   // Obtener el valor inicial del contador desde LocalStorage, o inicializar en 434 si no existe
   let contador = localStorage.getItem("contadorVisitas") || 434;
+  
   // Mostrar el valor del contador en la página
   document.getElementById("contador").textContent = contador;
+  
   // Función que incrementa el contador y lo guarda en LocalStorage
   function incrementarContador() {
-    contador++;
-    document.getElementById("contador").textContent = contador;
-    localStorage.setItem("contadorVisitas", contador); // Guardar el contador en LocalStorage
+      contador++;
+      document.getElementById("contador").textContent = contador;
+      localStorage.setItem("contadorVisitas", contador); // Guardar el contador en LocalStorage
   }
+ 
   // Asignar la función al botón
-  document
-    .getElementById("contadorBtn")
-    .addEventListener("click", incrementarContador);
+  document.getElementById("contadorBtn").addEventListener("click", incrementarContador);
 };
